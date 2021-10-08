2cbd8beda0b343b97d903b35fd2e97e6c1218b8f
Hashir Umar <hashirumar19@gmail.com>
2021-10-08 16:00:31 +0500

    initial commit

---
+++ src/components/Timer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Timer = (props) => {
+  return <div>Remaining Time: 10 minutes 10 seconds</div>;
+};
+
+export default Timer;
---
+++ src/pages/Quiz/AttemptQuiz/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Timer from "../../../components/Timer";
+
+const AttemptQuiz = (props) => {
+  return (
+    <div>
+      <h2>Attempt Quiz Page</h2>
+      <Timer />
+    </div>
+  );
+};
+
+export default AttemptQuiz;
